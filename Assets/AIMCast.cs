d263b5e8d1cdf9a6a54c240fe3aaf2ac80917308
min <7kyeongmin@gmail.com>
2018-12-19 12:26:16 +0900

    이벤트 전송으로 VR 클릭

--- Assets/AIMCast.cs
+++ Assets/AIMCast.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;     //?? 캔버스 용? 캔버스 내 이벤트 전달 관련 코드들이 들어잇다.
 
 public class AIMCast : MonoBehaviour {
     public float dist = 10.0f;
@@ -17,7 +18,6 @@ public class AIMCast : MonoBehaviour {
 	void Start () {
         tr = GetComponent<Transform>();
 	}
-	
 	void Update () {
         ray = new Ray(tr.position, tr.forward * dist);
         //if(Physics.Raycast(ray, out hit, 1 << 9 | 1 << 10))
@@ -33,6 +33,29 @@ public class AIMCast : MonoBehaviour {
             aim.isGaze = false;
             aimCircle.isGaze = false;
         }
-		
+        CheckGaze();
 	}
+    void CheckGaze()
+    {
+        //레이이용해서 이벤트 전달하기
+        PointerEventData data = new PointerEventData(EventSystem.current);
+        if(Physics.Raycast(ray, out hit, dist, 1<<9 | 1 << 10))
+        {
+            currObj = hit.collider.gameObject;  //히트된 오브젝트
+            if(currObj != prevObj)      //히드된 오브젝트가 처음 바라봣을때 엔터 핸들러 ! 해서, 호출? 두번짼 이미 바라보는것이 들어가 있기에, 통과!
+            {
+                ExecuteEvents.Execute(currObj, data, ExecuteEvents.pointerEnterHandler);
+                ExecuteEvents.Execute(currObj, data, ExecuteEvents.pointerExitHandler);
+                prevObj = currObj;  //바라보는 것을을 넣었어,
+            }
+        }
+        else
+        {
+            if(prevObj != null)
+            {
+                ExecuteEvents.Execute(prevObj, data, ExecuteEvents.pointerExitHandler);
+                prevObj = null;
+            }
+        }
+    }
 }
--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Item : MonoBehaviour {
+    public GameObject canvasObj;   //
+    private void Start()
+    {
+        canvasObj.SetActive(false);
+    }
 
     public void OnPointEnter(bool isEnter)
     {
         if (isEnter)
         {
-            AutoMove.isStopped = true;
+            canvasObj.SetActive(true);
+            //AutoMove.isStopped = true;    //Aim 에서 처리중이다
             //Debug.Log("Enter");
         }
         else
         {
-            AutoMove.isStopped = false;
+            //AutoMove.isStopped = false;   //Aim 에서 처리중이다
             //Debug.Log("Exit");
         }
     }
